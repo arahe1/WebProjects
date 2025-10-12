6139f13d4c9bb8dfbe5c00667dcf323c52357024
Alex <alexrahe2@gmail.com>
2025-10-11 20:40:22 -0500

    Introducing a more modular style to the code base

--- Fantasy_Calculator.py
+++ Fantasy_Calculator.py
@@ -1,5 +1,32 @@
-from Weekly_Webpage_Creator import All_DataFrames
+import pandas as pd
+import numpy as np
+import subprocess
+from collections import defaultdict
+from IPython.display import HTML, display
+import requests
+from bs4 import BeautifulSoup
+import ast
+pd.set_option('display.max_columns', None)
+from Imports import CSV_Importer as ci
+from Imports import Schedule_Maker as sm
+from Imports import Total_Stats_Combiner as tsc
+from Imports import Create_Individual_Totals as cit
+from Imports import Create_Useful as cu
+from Imports import Create_Team_Totals as ctt
+from Imports import Weekly_Final_Dataframes as wfd
+from Imports import Create_Weekly_HTML as wrh
+from Imports import Injury_Removal as ir
 
+listicle = ['CSVs/Week_1_NFL_2025.csv','CSVs/Week_2_NFL_2025.csv','CSVs/Week_3_NFL_2025.csv','CSVs/Week_4_NFL_2025.csv','CSVs/Week_5_NFL_2025.csv']
+DFs = ci.importstats(listicle)
+Schedule = sm.schedulemaker('CSVs/Schedule_2025.csv')
+Week = len(DFs)+1
+Total_Stats = tsc.totalstatcombiner(DFs)
+IndividualTotals = cit.individualtotals(DFs)
+Useful = cu.usefulstats(DFs, Week, Schedule, Total_Stats, IndividualTotals)
+Useful = ir.injuryremoval(Useful)
+TeamTotals = ctt.teamtotals(DFs, Schedule)
+All_DataFrames = wfd.weeklyfinaldataframes(Useful, TeamTotals)
 
 SuperFlex = All_DataFrames["SuperFlex"]
 GC_Total = []
@@ -7,7 +34,7 @@ Noche_Total = []
 Paint_Total = []
 Jerks_Total = []
 n = 0
-while n < 10:
+while n < 1000:
 
     #GC Score
     GC_Team = ['Patrick Mahomes', 'A.J. Brown', 'Brian Thomas', 'Xavier Worthy', 'Alvin Kamara', 'Rhamondre Stevenson', 'Sam LaPorta', 'Troy Franklin']
@@ -64,4 +91,6 @@ while n < 10:
 
 
 
-print(f"'GC Score    =', {GC_Total.mean().round(2)}\n, 'Noche Score =', {Noche_Total.mean().round(2)}\n, 'Paint Score =', {Paint_Total.mean().round(2)}\n, 'Jerks Score =', {Jerks_Total.mean().round(2)}")
+print(f"GC Score    = {round(sum(GC_Total)/len(GC_Total), 2)}\n Noche Score = {round(sum(Noche_Total)/len(Noche_Total), 2)}\n Paint Score = {round(sum(Paint_Total)/len(Paint_Total), 2)}\n Jerks Score = {round(sum(Jerks_Total)/len(Jerks_Total), 2)}")
+
+

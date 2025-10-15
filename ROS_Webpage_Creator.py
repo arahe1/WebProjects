bfa3bfa2a9ac0dfce035b9226aa7c77f00ab6567
Alex <alexrahe2@gmail.com>
2025-10-15 10:25:50 -0500

    Week 6 Updates

--- ROS_Webpage_Creator.py
+++ ROS_Webpage_Creator.py
@@ -6,7 +6,12 @@ import subprocess
 pd.set_option('display.max_columns', None)
 from Imports import PYScripts as ps
 
-listicle = ['CSVs/Week_1_NFL_2025.csv','CSVs/Week_2_NFL_2025.csv','CSVs/Week_3_NFL_2025.csv','CSVs/Week_4_NFL_2025.csv','CSVs/Week_5_NFL_2025.csv']
+listicle = ['CSVs/Week_1_NFL_2025.csv',
+            'CSVs/Week_2_NFL_2025.csv',
+            'CSVs/Week_3_NFL_2025.csv',
+            'CSVs/Week_4_NFL_2025.csv',
+            'CSVs/Week_5_NFL_2025.csv',
+            'CSVs/Week_6_NFL_2025.csv']
 DFs = ps.importstats(listicle)
 Schedule = ps.schedulemaker('CSVs/Schedule_2025.csv')
 Week = len(DFs)+1

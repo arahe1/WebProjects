84d76ca7bba5d2b7919e14678ff2f40d2265f1d9
Alex <alexrahe2@gmail.com>
2025-10-22 15:03:15 -0500

    Adding data and producing predtictions for Week 7

--- Weekly_Webpage_Creator.py
+++ Weekly_Webpage_Creator.py
@@ -14,7 +14,8 @@ listicle = ['CSVs/Week_1_NFL_2025.csv',
             'CSVs/Week_3_NFL_2025.csv',
             'CSVs/Week_4_NFL_2025.csv',
             'CSVs/Week_5_NFL_2025.csv',
-            'CSVs/Week_6_NFL_2025.csv']
+            'CSVs/Week_6_NFL_2025.csv',
+            'CSVs/Week_7_NFL_2025.csv']
 DFs = ps.importstats(listicle)
 Schedule = ps.schedulemaker('CSVs/Schedule_2025.csv')
 Week = len(DFs)+1

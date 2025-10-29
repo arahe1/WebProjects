7e6feffeb0c0658acb28dd9111d8aa55e7a8f265
Alex <alexrahe2@gmail.com>
2025-10-29 09:42:13 -0500

    Adding data and producing predtictions for Week 8

--- Fantasy_Calculator.py
+++ Fantasy_Calculator.py
@@ -25,33 +25,46 @@ except Exception as e:
 
 #GC Score
 GC_QB = 'Patrick Mahomes'
-GC_WR1 = 'A.J. Brown'
+GC_WR1 = 'Brian Thomas'
 GC_WR2 = 'Xavier Worthy'
 GC_WR3 = 'Terry McLaurin'
 GC_RB1 = 'Alvin Kamara'
 GC_RB2 = 'Kimani Vidal'
-GC_TE = 'Dalton Schultz'
+GC_TE = 'Sam LaPorta'
 GC_Flex = 'Rhamondre Stevenson'
-GC_Bench1 = 'Sam LaPorta'
-GC_Bench2 = 'Brian Thomas'
+GC_Bench1 = 'Dalton Schultz'
+GC_Bench2 = 'AJ Brown'
 GC_Bench3 = 'Bucky Irving'
 GC_Bench4 = 'Brian Robinson'
-GC_Bench5 = 'Tez Johnson'
+GC_Bench5 = 'Elic Ayomanor'
 GC_IR = 'Trey Benson'
-GC_DEF_approx = 7.5
+GC_DEF_approx = 9.43
 GC_Team = [GC_QB, GC_WR1, GC_WR2, GC_WR3, GC_RB1, GC_RB2, GC_TE, GC_Flex]
+GC_Bench = [GC_Bench1, GC_Bench2, GC_Bench3, GC_Bench4, GC_Bench5, GC_IR]
 GC_Score = 0
-GC_DF = []
+GC_ScoreDF = []
+GC_BenchDF = []
 for player in GC_Team:
     if player in df['Player'].values:  
         GC_Score += df.loc[df['Player'] == player, 'PPR'].iloc[0]
         row = {'Player': player, 'Points': float(df.loc[df['Player'] == player, 'PPR'].iloc[0])}
-        GC_DF.append(row)
+        GC_ScoreDF.append(row)
+    else:
+        print(f"{player} not found in the DataFrame.")
+        sys.exit()
+for player in GC_Bench:
+    if player in df['Player'].values:  
+        row = {'Player': player, 'Points': float(df.loc[df['Player'] == player, 'PPR'].iloc[0])}
+        GC_BenchDF.append(row)
+    elif player == '':
+        row = {'Player': player, 'Points': 0}
+        GC_BenchDF.append(row) 
     else:
         print(f"{player} not found in the DataFrame.")
         sys.exit()
 print(f"\n GC Score    = {round(GC_Score+GC_DEF_approx, 2)} \n")
-pprint(GC_DF)
+pprint(GC_ScoreDF)
+pprint(GC_BenchDF)
 
 
 #Noche Score
@@ -59,95 +72,149 @@ Noche_QB1 = 'Bo Nix'
 Noche_WR1 = "Ja'Marr Chase"
 Noche_WR2 = 'Stefon Diggs'
 Noche_WR3 = 'Justin Jefferson'
-Noche_RB1 = 'Cam Skattebo'
+Noche_RB1 = 'Kyren Williams'
 Noche_RB2 = 'Alvin Kamara'
-Noche_TE = 'Dallas Goedert'
-Noche_Flex = 'Jordan Mason'
-Noche_SuperFlex = 'Cade Otton'
-Noche_Bench1 = 'Hunter Henry'
-Noche_Bench2 = 'Jared Goff'
-Noche_Bench3 = 'Kyren Williams'
+Noche_TE = 'Hunter Henry'
+Noche_Flex = 'Zonovan Knight'
+Noche_SuperFlex = 'Jared Goff'
+Noche_Bench1 = 'Dallas Goedert'
+Noche_Bench2 = 'Kareem Hunt'
+Noche_Bench3 = 'Jordan Mason'
 Noche_Bench4 = 'Cooper Kupp'
+Noche_IR = ''
 Noche_DEF_approx = 14.33
 Noche_Team = [Noche_QB1, Noche_WR1, Noche_WR2, Noche_WR3, Noche_RB1, Noche_RB2, Noche_TE, Noche_Flex, Noche_SuperFlex]
+Noche_Bench = [Noche_Bench1, Noche_Bench2, Noche_Bench3, Noche_Bench4, Noche_IR]
 Noche_Score = 0
-Noche_DF = []
+Noche_ScoreDF = []
+Noche_BenchDF = []
 for player in Noche_Team:
     if player in df['Player'].values:  
         Noche_Score += df.loc[df['Player'] == player, 'PPR'].iloc[0]
         row = {'Player': player, 'Points': float(df.loc[df['Player'] == player, 'PPR'].iloc[0])}
-        Noche_DF.append(row)    
+        Noche_ScoreDF.append(row)    
+    else:
+        print(f"{player} not found in the DataFrame.")
+        sys.exit()
+for player in Noche_Bench:
+    if player in df['Player'].values:  
+        row = {'Player': player, 'Points': float(df.loc[df['Player'] == player, 'PPR'].iloc[0])}
+        Noche_BenchDF.append(row)   
+    elif player == '':
+        row = {'Player': player, 'Points': 0}
+        Noche_BenchDF.append(row) 
     else:
         print(f"{player} not found in the DataFrame.")
         sys.exit()
 print(f"\n Noche Score = {round(Noche_Score+Noche_DEF_approx, 2)} \n")
-pprint(Noche_DF)
+pprint(Noche_ScoreDF)
+pprint(Noche_BenchDF)
 
 
 #Paint Score
-Paint_QB = 'Michael Penix'
+Paint_QB = 'Jared Goff'
 Paint_WR1 = 'Nico Collins'
-Paint_WR2 = 'Rashid Shaheed'
-Paint_WR3 = 'D.J. Moore'
+Paint_WR2 = 'Jaxon Smith-Njigba'
+Paint_WR3 = 'Puka Nacua'
 Paint_RB1 = 'Jonathan Taylor'
-Paint_RB2 = 'Breece Hall'
-Paint_TE = 'Dallas Goedert'
-Paint_Flex = 'Rachaad White'
-Paint_Bench1 = 'Jared Goff'
-Paint_Bench2 = 'Jaxon Smith-Njigba'
-Paint_Bench3 = 'Puka Nacua'
+Paint_RB2 = 'Kimani Vidal'
+Paint_TE = 'Oronde Gadsden'
+Paint_Flex = 'Zonovan Knight'
+Paint_Bench1 = 'Rashid Shaheed'
+Paint_Bench2 = 'Breece Hall'
+Paint_Bench3 = 'Dallas Goedert'
 Paint_Bench4 = 'Rico Dowdle'
-Paint_Bench5 = 'Kimani Vidal'
+Paint_Bench5 = 'Rachaad White'
 Paint_IR = 'Trey Benson'
 Paint_K_approx = 8.60
 Paint_DEF_approx = 16.00
 Paint_Team = [Paint_QB, Paint_WR1, Paint_WR2, Paint_WR3, Paint_RB1, Paint_RB2, Paint_TE, Paint_Flex]
+Paint_Bench = [Paint_Bench1, Paint_Bench2, Paint_Bench3, Paint_Bench4, Paint_Bench5, Paint_IR]
 Paint_Score = 0
-Paint_DF = []
+Paint_ScoreDF = []
+Paint_BenchDF = []
 for player in Paint_Team:
     if player in df['Player'].values:  
         Paint_Score += df.loc[df['Player'] == player, 'STD'].iloc[0]
         row = {'Player': player, 'Points': float(df.loc[df['Player'] == player, 'PPR'].iloc[0])}
-        Paint_DF.append(row)    
+        Paint_ScoreDF.append(row)    
+    else:
+        print(f"{player} not found in the DataFrame.")
+        sys.exit()
+for player in Paint_Bench:
+    if player in df['Player'].values:  
+        row = {'Player': player, 'Points': float(df.loc[df['Player'] == player, 'PPR'].iloc[0])}
+        Paint_BenchDF.append(row)   
+    elif player == '':
+        row = {'Player': player, 'Points': 0}
+        Paint_BenchDF.append(row) 
     else:
         print(f"{player} not found in the DataFrame.")
         sys.exit()
 print(f"\n Paint Score = {round(Paint_Score+Paint_K_approx+Paint_DEF_approx, 2)} \n")
-pprint(Paint_DF)
+pprint(Paint_ScoreDF)
+pprint(Paint_BenchDF)
+
 
 #Jerks Score
 Jerks_QB1 = 'Jaxson Dart'
-Jerks_QB2 = 'Carson Wentz'
+Jerks_QB2 = 'Jayden Daniels'
 Jerks_WR1 = 'Stefon Diggs'
 Jerks_WR2 = 'Quentin Johnston'
-Jerks_WR3 = 'Alec Pierce'
-Jerks_RB1 = 'Jordan Mason'
+Jerks_WR3 = 'Marvin Harrison Jr.'
+Jerks_RB1 = 'Jahmyr Gibbs'
 Jerks_RB2 = 'James Cook'
-Jerks_TEFlex = 'Cade Otton'
+Jerks_TEFlex = 'Brock Bowers'
 Jerks_Flex1 = 'J.K. Dobbins'
 Jerks_Flex2 = 'Xavier Worthy'
-Jerks_Flex3 = 'Rachaad White'
-Jerks_Flex4 = "D'Andre Swift"
-Jerks_Flex5 = 'Calvin Ridley'
-Jerks_Bench1 = 'Jayden Daniels'
-Jerks_Bench2 = 'Jahmyr Gibbs'
-Jerks_Bench3 = 'Marvin Harrison Jr.'
-Jerks_IR = 'Brock Bowers'
+Jerks_Flex3 = "D'Andre Swift"
+Jerks_Flex4 = 'Alec Pierce'
+Jerks_Flex5 = 'Tyrone Tracy Jr.'
+Jerks_Bench1 = 'Zonovan Knight'
+Jerks_Bench2 = 'Cade Otton'
+Jerks_Bench3 = 'Rachaad White'
+Jerks_IR = ''
 Jerks_Team = [Jerks_QB1, Jerks_QB2, Jerks_WR1, Jerks_WR2, Jerks_WR3, Jerks_RB1, Jerks_RB2, Jerks_TEFlex, Jerks_Flex1, Jerks_Flex2, Jerks_Flex3, Jerks_Flex4, Jerks_Flex5]
+Jerks_Bench = [Jerks_Bench1, Jerks_Bench2, Jerks_Bench3, Jerks_IR]
 Jerks_Score = 0
-Jerks_DF = []
+Jerks_ScoreDF = []
+Jerks_BenchDF = []
 for player in Jerks_Team:
     if player in df['Player'].values:  
         Jerks_Score += df.loc[df['Player'] == player, 'PPR'].iloc[0]
         row = {'Player': player, 'Points': float(df.loc[df['Player'] == player, 'PPR'].iloc[0])}
-        Jerks_DF.append(row)    
+        Jerks_ScoreDF.append(row)    
+    else:
+        print(f"{player} not found in the DataFrame.")
+        sys.exit()
+for player in Jerks_Bench:
+    if player in df['Player'].values:  
+        row = {'Player': player, 'Points': float(df.loc[df['Player'] == player, 'PPR'].iloc[0])}
+        Jerks_BenchDF.append(row)
+    elif player == '':
+        row = {'Player': player, 'Points': 0}
+        Jerks_BenchDF.append(row) 
     else:
         print(f"{player} not found in the DataFrame.")
         sys.exit()
 print(f"\n Jerks Score = {round(Jerks_Score, 2)} \n")
-pprint(Jerks_DF)
+pprint(Jerks_ScoreDF)
+pprint(Jerks_BenchDF)
 
 
 
+# File to save the outputs
+#output_file = "dataframes_log.txt"
 
+# Use append mode so each run adds to the same file
+#with open(output_file, "a") as f:
+#    f.write("\n" + "="*50 + "\n")
+#    f.write(f"Run on {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+#    f.write("="*50 + "\n\n")
+    
+#    f.write("DataFrame 1:\n")
+#    f.write(df1.to_string(index=False))  # Convert DataFrame to text
+#    f.write("\n\nDataFrame 2:\n")
+#    f.write(df2.to_string(index=False))
+#    f.write("\n\n")
 

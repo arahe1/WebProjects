6981f54b0ed6f233c8168c206deb77b9a161e9b0
Alex <alexrahe2@gmail.com>
2025-11-02 13:01:29 -0600

    Adding data for Week 8 and Producing predictions for Rest Of Season

--- Imports/PYScripts.py
+++ Imports/PYScripts.py
@@ -1082,6 +1082,7 @@ def roshtml(alldataframes):
         <html>
         <head>
         <title>{name} Stats</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png" />
         <link rel="icon" type="image/svg+xml" href="images/favicon.svg" />
         <link rel="shortcut icon" href="images/favicon.ico" />

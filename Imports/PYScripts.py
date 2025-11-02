a894bd3f3a1d5302087bfc56decfc28a046d89cf
Alex <alexrahe2@gmail.com>
2025-11-02 10:49:37 -0600

    Adding data and producing predtictions for Week 8

--- Imports/PYScripts.py
+++ Imports/PYScripts.py
@@ -533,7 +533,9 @@ def injuryremovalweekly(superflex):
 
     #print(IR_Players)
 
-    superflex.loc[superflex['Player'].isin(IR_Players), ~superflex.columns.isin(['Player', 'Team', 'Rank', 'Pos.'])] = 0
+    #superflex.loc[superflex['Player'].isin(IR_Players), ~superflex.columns.isin(['Player', 'Team', 'Rank', 'Pos.'])] = 0
+    superflex.loc[superflex['Player'].isin(IR_Players), [c for c in superflex.columns if c not in ['Player', 'Team', 'Rank', 'Pos.']]] = 0
+
     #Useful = useful[~useful['Player'].isin(IR_Players)]
     #SuperFlex = SuperFlex[~SuperFlex['Player'].isin(IR_Players)]
     #Flex = Flex[~Flex['Player'].isin(IR_Players)]

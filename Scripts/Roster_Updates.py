5338b470ae0b87d8494ee14b134cb694ed9b6b49
Alex <alexrahe2@gmail.com>
2026-08-07 18:05:37 -0500

    Update Roster_Updates.py

--- Scripts/Roster_Updates.py
+++ Scripts/Roster_Updates.py
@@ -8,11 +8,8 @@ listicle = get_nfl_week_files(2025, folder="CSVs")
 DFs = ps.importstats(listicle)
 prevdepth = build_depth_chart(DFs, 2025)
 newrosters = get_preseason_rosters(2026)
-Schedule = ps.schedulemaker('CSVs/Schedule_2025.csv')
-Week = len(DFs)+1
 Total_Stats = ps.totalstatcombiner(DFs)
 IndividualTotals = ps.individualtotals(DFs)
-Useful = ps.usefulstats(DFs, Week, Schedule, Total_Stats, IndividualTotals)
-Dominance = ps.analysis(Useful,IndividualTotals)['FlexDom']
+Dominance = ps.analysis(Total_Stats,IndividualTotals)['FlexDom']
 updated_depth = update_depth_chart(prevdepth, newrosters, Dominance)
 

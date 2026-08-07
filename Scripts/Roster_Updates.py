57508bfe621cdf6ffa98f85c34034a75cafbc6d6
Alex <alexrahe2@gmail.com>
2026-08-07 18:24:17 -0500

    Update Roster_Updates.py
    Add draft to workflow

--- Scripts/Roster_Updates.py
+++ Scripts/Roster_Updates.py
@@ -4,14 +4,15 @@ import subprocess
 pd.set_option('display.max_columns', None)
 from Imports import PyFunc as ps
 
-listicle = get_nfl_week_files(2025, folder="CSVs")
+listicle = ps.get_nfl_week_files(2025, folder="CSVs")
 DFs = ps.importstats(listicle)
-prevdepth = build_depth_chart(DFs, 2025)
-newrosters = get_preseason_rosters(2026)
+prevdepth = ps.build_depth_chart(DFs, 2025)
+newrosters = ps.get_preseason_rosters(2026)
 Total_Stats = ps.totalstatcombiner(DFs)
 IndividualTotals = ps.individualtotals(DFs)
 Dominance = ps.analysis(Total_Stats,IndividualTotals)['FlexDom']
-updated_depth = update_depth_chart(prevdepth, newrosters, Dominance)
+Draft = ps.get_nfl_draft(year)
+updated_depth = ps.update_depth_chart(prevdepth, newrosters, Dominance)
 
 updated_depth.to_csv("Preseason_DepthChart_2026.csv", index=False)
 

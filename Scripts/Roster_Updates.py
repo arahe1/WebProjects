d647e58c7506e08d60df957f055b95570e80caf2
Alex <alexrahe2@gmail.com>
2026-08-07 17:39:31 -0500

    Update Roster_Updates.py
    Roster update script made

--- Scripts/Roster_Updates.py
+++ Scripts/Roster_Updates.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import os
+import subprocess
+pd.set_option('display.max_columns', None)
+from Imports import PyFunc as ps
+
+listicle = get_nfl_week_files(2025, folder="CSVs")
+DFs = ps.importstats(listicle)
+prevdepth = build_depth_chart(DFs, 2025)
+newrosters = get_preseason_rosters(2026)
+Schedule = ps.schedulemaker('CSVs/Schedule_2025.csv')
+Week = len(DFs)+1
+Total_Stats = ps.totalstatcombiner(DFs)
+IndividualTotals = ps.individualtotals(DFs)
+Useful = ps.usefulstats(DFs, Week, Schedule, Total_Stats, IndividualTotals)
+Dominance = ps.analysis(Useful,IndividualTotals)
+updated_depth = update_depth_chart(prevdepth, newrosters, off_focus_df)
 
